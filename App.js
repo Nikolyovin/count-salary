bbf1108ffa1fad8ea9e6568051051e276f7fd6e0
Nikolyovin <pulya0763@gmail.com>
2022-10-05 23:22:08 +0300

    button add

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 import Footer from './src/components/footer/Footer';
 import Header from './src/components/header/Header';
 import Main from './src/components/main/Main';
@@ -7,16 +7,27 @@ import Main from './src/components/main/Main';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Header/>
-      <Main/>
-      <Footer/>
-      <StatusBar style="auto" />
+      <View style={styles.mainFooter}>
+        <StatusBar style="auto" />
+        <Header />
+        <Main />
+      </View>
+      <Footer />
+
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    
+    // flex: 1,
+    paddingTop: 30,
+
+    height: '100%',
+    // backgroundColor: 'green'
+    // justifyContent: 'space-around'
   },
+  mainFooter: {
+    flex: 1
+  }
 })
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,14 @@
-import { Button, StyleSheet, View } from 'react-native'
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 
 const Footer = () => {
     return (
-        <View>
-            <Button style = { styles.button } title = 'Добавить'/>
+        <View style = { styles.footer }>
+            <View style = { styles.buttonWrap }>
+                <TouchableOpacity style = { styles.button } >
+                    <Text>Добавить</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
@@ -12,7 +16,19 @@ const Footer = () => {
 export default Footer
 
 const styles = StyleSheet.create({
+    footer: {
+        paddingBottom: 30
+    },
+    buttonWrap: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        
+    },
     button: {
+        backgroundColor: '#1976d2',
+        width: 130,
+        height: 35,
+        borderWidth: 1,
         alignItems: 'center',
         justifyContent: 'center',
     }
